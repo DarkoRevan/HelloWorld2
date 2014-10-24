8fa02408e284e22b3d1f5f86fecdfab9931839c9
DarkoRevan <Darko.Revan@yahoo.de>
2014-10-24 05:04:39 +0200

    removed line 7

--- Hello World 2/src/Main.java
+++ Hello World 2/src/Main.java
@@ -4,7 +4,6 @@ class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Are you Darko Revan?");
-		System.out.println("Seems like you're not.... BYE...");
 	}
 
 }
