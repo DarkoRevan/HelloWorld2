83a59761f9bf2dd5f0bc011ad07a76f5d7d57d4a
DarkoRevan <Darko.Revan@yahoo.de>
2014-10-24 05:28:24 +0200

    added option to answer

--- Hello World 2/src/Main.java
+++ Hello World 2/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Are you Darko Revan?");
+		Scanner input = new Scanner(System.in);
+		if (input.nextLine().equals("y"))
+			System.out.println("Hi Darko...");
+		else
+			System.out.println("Not Darko? BYE");
 	}
 
 }
